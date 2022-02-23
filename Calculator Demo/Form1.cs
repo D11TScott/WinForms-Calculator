6bfebf86217956e89c5579ca2b48c49d9f32c452
D11TScott <t.scott@double11.com>
2022-02-23 12:11:48 +0000

    Displaying text to the text box
    The onclick events now display the relevant text to the textbox at the top of the calculator

--- Calculator Demo/Form1.cs
+++ Calculator Demo/Form1.cs
@@ -19,7 +19,14 @@ namespace Calculator_Demo
 
         private void btnCopy_Click(object sender, EventArgs e)
         {
-            Clipboard.SetText(textDisplay.Text);
+            if (textDisplay.Text.Length > 0)
+            {
+                Clipboard.SetText(textDisplay.Text);
+            }
+            else
+            {
+                Clipboard.SetText("");
+            }
         }
 
         private void btnReset_Click(object sender, EventArgs e)
@@ -37,27 +44,27 @@ namespace Calculator_Demo
 
         private void btnDiv_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += " ÷ ";
         }
 
         private void btnMult_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += " x ";
         }
 
         private void btnSub_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += " - ";
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += " + ";
         }
 
         private void btnEquals_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += " = ";
         }
 
         private void btnPrime_Click(object sender, EventArgs e)
@@ -67,52 +74,52 @@ namespace Calculator_Demo
 
         private void btn0_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "0";
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "1";
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "2";
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "3";
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "4";
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "5";
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "6";
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "7";
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "8";
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-
+            textDisplay.Text += "9";
         }
     }
 }
